d1d237d495ab846eeacafcba18bc394839da6aec
Hanzheng Lyu <93515412+Hanzheng2021@users.noreply.github.com>
2022-06-14 19:21:52 +0800

    Update Check if Binary String Has at Most One Segment of Ones.go

--- Check if Binary String Has at Most One Segment of Ones.go
+++ Check if Binary String Has at Most One Segment of Ones.go
@@ -1,22 +1,6 @@
 func checkOnesSegment(s string) bool { 
-    if s=="1" {
-        return true
-    }
-    sss := strings.Split(s,"")
-
-    j:=0
-    for i:=1; i<len(sss); i++{
-        if sss[0]=="1"&&sss[i]=="0"{
-            j++
-        }
-    }
-    fmt.Println(j)
-    if j==len(sss)-1 {
-        return true
-    }
     ss := strings.Split(s,"0")
     fmt.Println(ss)
-
 
     k := 0
     for _,v := range ss {
@@ -24,8 +8,8 @@ func checkOnesSegment(s string) bool {
             k++
         }
     }
-    fmt.Println(k)
-    if k == 1 && ss[0]!= "1" && ss[len(ss)-1]!= "1" {
+
+    if k == 1 {
         return true
     } else {
         return false
