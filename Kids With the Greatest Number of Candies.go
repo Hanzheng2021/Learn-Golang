b14781b598d82f6ebc2d4207d127e16d90d8ced5
Hanzheng Lyu <93515412+Hanzheng2021@users.noreply.github.com>
2022-06-28 16:58:16 +0800

    Update Kids With the Greatest Number of Candies.go

--- Kids With the Greatest Number of Candies.go
+++ Kids With the Greatest Number of Candies.go
@@ -1,1 +1,19 @@
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+    var result []bool
+    maxcandies := 0
+    for _,v := range candies {
+        if v>maxcandies {
+            maxcandies = v
+        }
+    }
 
+    for _,v := range candies {
+        if v+extraCandies>=maxcandies {
+            result = append(result,true) 
+        } else {
+            result = append(result,false)
+        }
+    }
+
+    return result
+}
